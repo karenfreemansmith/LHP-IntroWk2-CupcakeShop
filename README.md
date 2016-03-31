4600b1ff7317fcf78a407f63e7d909bbfe9c49d0
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-30 19:57:13 -0700

    added conversion forms to recipe page and address to receipt

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 _by Karen Freeman-Smith_
 
 # Description
-This website was built to practice using CSS elements, it has been updated with Bootstrap and features an order form built with jQuery.
+This website was built to practice using CSS elements, it has been updated with Bootstrap and features an order form built with jQuery, and cooking conversions on the recipe page.
 
 # Technologies Used:
 HTML, CSS, Bootstrap, JavaScript, jQuery
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,48 @@
+var celsiusToFarenheight = function(degrees) {
+  return ((degrees*9)/5)+32;
+};
+var farenheightToCelsius = function(degrees) {
+  return ((degrees-32)*5)/9;
+};
+var gallonsToQuarts = function(gallons) {
+  return gallons*4;
+};
+var quartsToPints = function(quarts) {
+  return quarts*2;
+};
+var pintsToCups = function(pints) {
+  return pints*2;
+};
+var cupsToOunces = function(cups) {
+  return cups*8;
+};
+var ozToLiters = function(ounces) {
+  return ounces/33.82;
+};
+
+var litersToOunces = function(liters) {
+  return liters*33.82;
+};
+var ouncesToCups = function(oz) {
+  return oz/8;
+};
+
 $(document).ready(function() {
+  $("#gal2liters").submit(function(event) {
+    event.preventDefault();
+    var gallonsInput = $("input#gallons").val();
+    $(".gal2liters").text(ozToLiters(cupsToOunces(pintsToCups(quartsToPints(gallonsToQuarts(gallonsInput))))));
+  });
+  $("#liters2cups").submit(function(event) {
+    event.preventDefault();
+    var litersInput = $("input#liters").val();
+    $(".liters2cups").text(ouncesToCups(litersToOunces(litersInput)));
+  });
+  $("#cup2Tbsp").submit(function(event) {
+    event.preventDefault();
+    var cupsInput = $("input#cups").val();
+    $(".cup2Tbsp").text(cupsToOunces(cupsInput)/2);
+  });
   $("#order form").submit(function(event) {
     event.preventDefault();
 
